ee01e8199cd1424572266e33d5d5f23d52279feb
Vadim Borovikov <vadim.borovikov@gmail.com>
2020-02-26 14:00:14 +0300

    Logging update

--- DaresGame.Bot.Web/Startup.cs
+++ DaresGame.Bot.Web/Startup.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Threading.Tasks;
-using DaresGame.Bot.Web.Logging;
-using DaresGame.Bot.Web.Models;
+﻿using DaresGame.Bot.Web.Models;
+using DaresGame.Bot.Web.Models.Data;
 using DaresGame.Bot.Web.Models.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace DaresGame.Bot.Web
 {
@@ -42,7 +39,7 @@ namespace DaresGame.Bot.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -55,28 +52,11 @@ namespace DaresGame.Bot.Web
                 app.UseHsts();
             }
 
-            string path = $"{env.WebRootPath}{LogName}";
-            loggerFactory.AddProvider(new FileLoggerProvider(path, LogLevel.Warning));
-            ILogger<FileLogger> logger = loggerFactory.CreateLogger<FileLogger>();
-
-            app.UseExceptionHandler(a => a.Run(c => HandleExceptionAsync(c, logger)));
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
             app.UseMvc(routes => routes.MapRoute("update", $"{Configuration["Token"]}/{{controller=Update}}/{{action=post}}"));
         }
-
-        private static Task HandleExceptionAsync(HttpContext context, ILogger logger)
-        {
-            var feature = context.Features.Get<IExceptionHandlerPathFeature>();
-            Exception exception = feature.Error;
-
-            logger.LogError(exception, exception.ToString());
-            return Task.CompletedTask;
-        }
-
-        private const string LogName = "log.txt";
     }
 }
