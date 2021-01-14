8f8296a306be44b884f2cb550ec1e86056666cc7
Vadim Borovikov <vadim.borovikov@gmail.com>
2021-01-14 23:00:01 +0300

    Disposable Service

--- DaresGameBot.Web/Models/Service.cs
+++ DaresGameBot.Web/Models/Service.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using GoogleSheetsManager;
@@ -6,18 +7,22 @@ using Newtonsoft.Json;
 
 namespace DaresGameBot.Web.Models
 {
-    internal sealed class Service : IHostedService
+    internal sealed class Service : IHostedService, IDisposable
     {
-        public Service(IBot bot) => _bot = bot;
-
-        public Task StartAsync(CancellationToken cancellationToken)
+        public Service(IBot bot)
         {
+            _bot = bot;
+
             string googleCredentialsJson = _bot.Config.GoogleCredentialsJson;
             if (string.IsNullOrWhiteSpace(googleCredentialsJson))
             {
                 googleCredentialsJson = JsonConvert.SerializeObject(_bot.Config.GoogleCredentials);
             }
             _googleSheetsProvider = new Provider(googleCredentialsJson, ApplicationName, _bot.Config.GoogleSheetId);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
             _bot.Initialize(_googleSheetsProvider);
 
             return _bot.Client.SetWebhookAsync(_bot.Config.Url, cancellationToken: cancellationToken);
@@ -25,12 +30,13 @@ namespace DaresGameBot.Web.Models
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            _googleSheetsProvider.Dispose();
             return _bot.Client.DeleteWebhookAsync(cancellationToken);
         }
 
+        public void Dispose() => _googleSheetsProvider?.Dispose();
+
         private readonly IBot _bot;
-        private Provider _googleSheetsProvider;
+        private readonly Provider _googleSheetsProvider;
 
         private const string ApplicationName = "DaresGameBot";
     }
