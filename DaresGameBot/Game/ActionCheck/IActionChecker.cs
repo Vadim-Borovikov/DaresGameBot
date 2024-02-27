069a5635aec5f5367d41aee63022b9c5f0439e30
Вадим Боровиков <vadim.borovikov@gmail.com>
2024-02-27 22:54:51 +0100

    Parameter renamed

--- DaresGameBot/Game/ActionCheck/IActionChecker.cs
+++ DaresGameBot/Game/ActionCheck/IActionChecker.cs
@@ -5,5 +5,5 @@ namespace DaresGameBot.Game.ActionCheck;
 
 internal interface IActionChecker
 {
-    public bool Check(Player player, CardAction card);
+    public bool Check(Player player, CardAction action);
 }
