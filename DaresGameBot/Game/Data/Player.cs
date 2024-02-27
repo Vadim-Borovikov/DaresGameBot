def37ab347fc749a540495d3f69a0d7bb6ee8493
Вадим Боровиков <vadim.borovikov@gmail.com>
2024-02-27 01:53:33 +0100

    Old commented code removed

--- DaresGameBot/Game/Data/Player.cs
+++ DaresGameBot/Game/Data/Player.cs
@@ -5,21 +5,4 @@ internal sealed class Player
     public readonly string Name;
 
     public Player(string name) => Name = name;
-
-    /*public static Player? From(string s)
-    {
-        string[] parts = s.Split(PartsSeparator);
-        return parts.Length != 3 ? null : new Player(parts[0], parts[1], parts[2].Split(GroupsSeparator));
-    }
-
-    private Player(string name, string group, IEnumerable<string> compatableGroups)
-    {
-        Name = name;
-
-        _group = group;
-        _compatableGroups = new HashSet<string>(compatableGroups);
-    }*/
-
-    /*private const string PartsSeparator = ",";
-    private const string GroupsSeparator = "+";*/
 }
