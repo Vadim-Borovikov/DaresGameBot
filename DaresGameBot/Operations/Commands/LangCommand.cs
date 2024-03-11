877df31870dacf80447692524805bb143d7ff3a5
Вадим Боровиков <vadim.borovikov@gmail.com>
2024-03-11 01:04:09 +0100

    LangCommand correction

--- DaresGameBot/Operations/Commands/LangCommand.cs
+++ DaresGameBot/Operations/Commands/LangCommand.cs
@@ -1,17 +1,16 @@
 ﻿using System.Threading.Tasks;
+using AbstractBot.Operations.Commands;
 using Telegram.Bot.Types;
 
 namespace DaresGameBot.Operations.Commands;
 
-internal sealed class LangCommand : DaresGameCommand
+internal sealed class LangCommand : CommandSimple
 {
     protected override byte Order => 6;
 
-    protected override string Alias => _bot.Config.Texts.NewGameCaption;
-
     public LangCommand(Bot bot) : base(bot, "lang", bot.Config.Texts.LangCommandDescription) => _bot = bot;
 
-    protected override Task ExecuteAsync(Chat chat, int _) => _bot.OnToggleLanguagesAsync(chat);
+    protected override Task ExecuteAsync(Message message, User sender) => _bot.OnToggleLanguagesAsync(message.Chat);
 
     private readonly Bot _bot;
 }
